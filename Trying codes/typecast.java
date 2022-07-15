5d48b5a9ecaeef94a6f13de17a593960f9a89713
AdarshRawat1 <adarshrawat146@gmail.com>
2022-07-15 23:42:56 +0530

    Initialising files

---
+++ Trying codes/typecast.java
@@ -0,0 +1,16 @@
+public class typecast
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println('a'+1);
+       /** 
+        char ch = 'a';
+        int ch2 = 'a' + 3;
+        ch= (char)ch2; 
+        System.out.println(ch);
+        ch2= ch +3;
+        System.out.println(ch2);
+        */
+    }
+}
