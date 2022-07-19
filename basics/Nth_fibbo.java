c2debb081f318b89bd8124577374732364487f19
AdarshRawat1 <adarshrawat146@gmail.com>
2022-07-19 22:58:17 +0530

    added Nth_fibbo.java

---
+++ basics/Nth_fibbo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Nth_fibbo{
+    public static void main (String[] Args){
+        Scanner sc =new Scanner ( System.in);
+        System.out.println("Enter the value of N ");
+        int n =sc.nextInt();
+        int c=1,n1=1,n2=1;
+        int t=0;
+        if (n==1){
+            System.out.println(n+"st term is 1"+n1);
+        }
+        else if (n==2){
+            System.out.println(n+"nd term is "+n2);
+        }
+        else if (n>=3){
+            c=2;
+            while (c!=n){
+                t = n1+n2;
+                n1=n2;
+                n2=t;
+                c++;
+            }
+            System.out.println(n+"th term is "+ t);
+        }
+        else {
+            System.out.println("Enter a valid value for n");
+        }
+        sc.close();
+    }
+}
